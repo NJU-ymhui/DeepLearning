9901f8f2853184897258112a4e48c604683a847b
yuminghui <221870120@smail.nju.edu.cn>
2024-10-01 14:25:14 +0800

    add ModernCNN, test all and modify

--- ModernCNN/tensor_normalize_self.py
+++ ModernCNN/tensor_normalize_self.py
@@ -4,11 +4,11 @@ from d2l import torch as d2l
 
 
 # 批量规范化
-def batch_norm(X, gamma, beta, moving_mean, moving_tar, eps, momentum):
+def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
     # 通过is_grad_enabled来判断是训练模式还是预测模式
     if not torch.is_grad_enabled():
         # 如果在预测模式下，直接使用传入的移动平均所获得的均值和方差
-        X_hat = (X - moving_mean) / torch.sqrt(moving_tar + eps)
+        X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
     else:
         assert len(X.shape) in (2, 4)
         if len(X.shape) == 2:
@@ -24,7 +24,7 @@ def batch_norm(X, gamma, beta, moving_mean, moving_tar, eps, momentum):
         X_hat = (X - mean) / torch.sqrt(var + eps)
         # 更新移动平均的均值和方差
         moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
-        moving_var = momentum * moving_tar + (1.0 - momentum) * var
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
     Y = gamma * X_hat + beta
     return Y, moving_mean.data, moving_var.data
 
